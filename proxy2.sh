8cccbf2258efb228ee5b339ec5228805a91d2747
Rafael R. Gaioso <rrgaioso@gmail.com>
2014-07-10 16:00:47 +0200

    Creado novo script para proxy rai. O script anterior pasa a chamarse proxy2.sh

--- proxy2.sh
+++ proxy2.sh
@@ -2,12 +2,12 @@
 # gardado en /etc/profile.d
 
 gsettings set org.gnome.system.proxy mode 'manual' 
-gsettings set org.gnome.system.proxy.http host 'proxy.rai.usc.es'
+gsettings set org.gnome.system.proxy.http host 'proxy2.usc.es'
 gsettings set org.gnome.system.proxy.http port 8080
-gsettings set org.gnome.system.proxy.ftp host 'proxy.rai.usc.es'
+gsettings set org.gnome.system.proxy.ftp host 'proxy2.usc.es'
 gsettings set org.gnome.system.proxy.ftp port 8080
-gsettings set org.gnome.system.proxy.https host 'proxy.rai.usc.es'
+gsettings set org.gnome.system.proxy.https host 'proxy2.usc.es'
 gsettings set org.gnome.system.proxy.https port 8080
-gsettings set org.gnome.system.proxy.socks host 'proxy.rai.usc.es'
+gsettings set org.gnome.system.proxy.socks host 'proxy2.usc.es'
 gsettings set org.gnome.system.proxy.socks port 8080
 gsettings set org.gnome.system.proxy ignore-hosts "['localhost','127.0.0.0/8','*.usc.es','usc.es' ]"
